f298372d1ac3ee9e9c20fddd0255357308889f52
Welington Brasil <welington.brasil@outlook.com>
2021-12-27 17:15:59 -0300

    Projeto sem a tratativa de exceção

---
+++ src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.next();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		Double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		Double withdrawLimit = sc.nextDouble();
+
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.print("\nEnter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		acc.withdraw(amount);
+		System.out.println("New balance: " + String.format("%.2f", acc.getBalance()));
+		
+		
+		sc.close();
+
+	}
+
+}
